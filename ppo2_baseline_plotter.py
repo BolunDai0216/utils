a7a90cc848599fd0bd4f9b9f09f5aea1694a624d
Bolun Dai <bolun@bolun-ThinkPad-P51.corp.safeai.ai>
2019-08-06 16:03:52 -0700

    added comments

--- ppo2_baseline_plotter.py
+++ ppo2_baseline_plotter.py
@@ -2,12 +2,35 @@ import argparse
 import re
 import matplotlib.pyplot as plt
 
+"""
+I have been finding it difficult to visualize the output of baselines,
+so I wrote something that can plot the reward or other stats when running ppo2
+
+When using this code run the OpenAI baselines ppo2 code using the following
+command:
+
+    python python -m baselines.run --alg=ppo2 --env=PongNoFrameskip-v4
+    --num_timesteps=2e7 --save_path=~/models/pong_20M_ppo2
+    > /some/dir/filename.txt
+
+And then when plotting the terminal output run the following command:
+
+    python ppo2_baseline_plotter.py --filename filename --keyword keyword
+
+And the plot should show up.
+"""
+
+# You can also specify the file and keyword here
+FILENAME = "/home/bolun/ppo2_ang_vel_disc_1e9_400_0806.txt"
+KEYWORD = "eprewmean"
+
 
 def parse_args():
     parser = argparse.ArgumentParser("Plot the output of baselines/ppo2")
-    parser.add_argument("--filename", type=str, default="/home/bolun/ppo2_ang_vel_disc_1e9_400_0806.txt",
+    parser.add_argument("--filename", type=str, default=FILENAME,
                         help="name of the output file")
-    parser.add_argument("--keyword", type=str, default="eprewmean", help="extracted data name")
+    parser.add_argument("--keyword", type=str, default=KEYWORD,
+                        help="extracted data name")
     return parser.parse_args()
 
 
